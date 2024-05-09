f44a250058d80748bfb37c506a376e8c3d78fa5d
Fabien Hinault <fabien.hinault@free.fr>
2024-05-09 17:11:26 +0200

    > constants* '(+ = -> term wff TT)

--- reader.rkt
+++ reader.rkt
@@ -1,9 +1,36 @@
 #lang br/quicklang
 
 (define (read-syntax path port)
-  (define src-datums (read port))
-  (define module-datum `(module metamath br
-                          '(,@src-datums)))
+  (define src-datums (port->list read port))
+  (define module-datum `(module metamath "reader.rkt"
+                          ,@src-datums))
   (datum->syntax #f module-datum))
 
 (provide read-syntax)
+
+
+
+(define-macro (metamath-module-begin EXPR ...)
+  #'(#%module-begin EXPR ...))
+(provide (rename-out [metamath-module-begin #%module-begin]))
+
+(define constants* '())
+(define variables* '())
+
+(define (add-constants constant-symbols)
+  (set! constants* (append constants* constant-symbols)))
+
+(define-syntax $c
+  (syntax-rules ()
+    (($c new-constant ...)
+     (add-constants (list 'new-constant ...)))))
+
+(define (add-variables variable-symbols)
+  (set! variables* (append variables* variable-symbols)))
+
+(define-syntax $v
+  (syntax-rules ()
+    (($c new-var ...)
+     (add-variables (list 'new-var ...)))))
+
+(provide $c add-constants $v add-variables constants* variables*)
--- test.rkt
+++ test.rkt
@@ -1,5 +1,7 @@
 #lang reader "reader.rkt"
 
 ($c + = -> term wff TT)
+($v u r s P Q)
+
 
 
