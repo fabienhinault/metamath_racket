4199c75ad3323d19493815cbd707e5d0dcdf2019
Fabien Hinault <fabien.hinault@free.fr>
2024-07-14 21:51:20 +0200

    module

---
+++ metamath/parse-only.rkt
@@ -0,0 +1,16 @@
+#lang br/quicklang
+(require "parser.rkt" "tokenizer.rkt")
+
+(define (read-syntax path port)
+  (define parse-tree (parse path (make-tokenizer port path)))
+  (strip-bindings
+   #`(module metamath-parser-mod metamath/parse-only
+       #,parse-tree)))
+(module+ reader (provide read-syntax))
+
+(define-macro (parser-only-mb PARSE-TREE)
+  #'(#%module-begin
+     'PARSE-TREE))
+(provide (rename-out [parser-only-mb #%module-begin]))
+
+
