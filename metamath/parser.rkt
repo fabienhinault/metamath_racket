bac6d1f4ff57114edc8c33490249ae30bb165908
fabienhinault <fabienhinault@users.noreply.github.com>
2024-07-17 14:51:10 +0200

    cuts and splices

--- metamath/parser.rkt
+++ metamath/parser.rkt
@@ -5,21 +5,22 @@
 
 ; see https://us.metamath.org/downloads/metamath.pdf p.211
 database : outermost-scope-stmt*
-outermost-scope-stmt : constant-stmt | stmt
-constant-stmt : "$c" constant+ "$."
-stmt : block | variable-stmt | disjoint-stmt | hypothesis-stmt | assert-stmt
-block : "${" stmt* "$}"
-variable-stmt : "$v" variable+ "$."
+@outermost-scope-stmt : constant-stmt | stmt
+constant-stmt : /"$c" constant+ /"$."
+@stmt : block | variable-stmt | disjoint-stmt | hypothesis-stmt | assert-stmt
+block : /"${" stmt* /"$}"
+variable-stmt : /"$v" variable+ /"$."
 disjoint-stmt : "$d" variable variable variable* "$."
-hypothesis-stmt : floating-stmt | essential-stmt
-floating-stmt : LABEL "$f" typecode variable "$."
-essential-stmt : LABEL "$e" typecode MATH-SYMBOL* "$."
-assert-stmt : axiom-stmt | provable-stmt
-axiom-stmt : LABEL "$a" typecode MATH-SYMBOL* "$."
-provable-stmt : LABEL "$p" typecode MATH-SYMBOL* "$=" proof "$."
+@hypothesis-stmt : floating-stmt | essential-stmt
+floating-stmt : LABEL /"$f" typecode variable /"$."
+essential-stmt : LABEL /"$e" typecode MATH-SYMBOL* /"$."
+@assert-stmt : axiom-stmt | provable-stmt
+axiom-stmt : LABEL /"$a" typecode MATH-SYMBOL* /"$."
+provable-stmt : LABEL /"$p" typecode MATH-SYMBOL* /"$=" proof /"$."
 proof : (LABEL | "?")+
-typecode : constant
-constant : MATH-SYMBOL
-variable : MATH-SYMBOL
-LABEL : _LABEL
-MATH-SYMBOL : _LABEL | _MATH-SYMBOL
+@typecode : constant
+@constant : MATH-SYMBOL
+@variable : MATH-SYMBOL
+@LABEL : _LABEL
+@MATH-SYMBOL : _LABEL | _MATH-SYMBOL
+
